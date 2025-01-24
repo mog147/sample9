83b6f2b678521ca12c169ccb17be86a1653f1e21
mog147 <396mog@gmail.com>
2025-01-24 15:13:22 +0900

    add comment

--- js/main.js
+++ js/main.js
@@ -16,3 +16,44 @@ if(window.matchMedia("(max-width: 768px)").matches){
       });
 
   }
+
+  $(function () {
+
+    var webStorage = function () {
+  
+      if (sessionStorage.getItem('access')) {
+  
+        /*
+          2回目以降アクセス時の処理
+        */
+  
+        $(".loading-animation").addClass('is-active'); // loadingアニメーションを表示
+        $(".loading").addClass('is-active');
+        
+  
+      } else {
+  
+        /*
+          初回アクセス時の処理
+        */
+  
+        sessionStorage.setItem('access', 'true'); // sessionStorageにデータを保存
+  
+        $(".loading-animation").addClass('is-active'); // loadingアニメーションを表示
+  
+        setTimeout(function () {
+  
+          // ローディングを数秒後に非表示にする
+  
+          $(".loading").addClass('is-active');
+          //$(".loading-animation").removeClass('is-active');
+  
+        }, 5000); // ローディングを表示する時間
+  
+      }
+  
+    }
+  
+    webStorage();
+    
+  });
